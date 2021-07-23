e91db10c38503575086c9f5c0d52db5c107406e5
MagentaSpruce <81999424+MagentaSpruce@users.noreply.github.com>
2021-07-23 18:23:28 -0400

    Context API setup
    Context was set up and used for searchTerm component and cocktail component. A message showing no results was created in the cocktail list.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum facere
+        odit quam deleniti blanditiis tempore, placeat neque totam. Fuga
+        consectetur ducimus ut reprehenderit reiciendis itaque voluptatem
+        assumenda voluptatum id in aliquam blanditiis quo cum aut quos
+        exercitationem, temporibus unde quisquam.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
