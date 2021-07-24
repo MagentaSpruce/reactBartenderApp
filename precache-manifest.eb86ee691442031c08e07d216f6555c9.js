a3533addc1acb9cf35fd1bfa545d220aaf7c40e7
MagentaSpruce <81999424+MagentaSpruce@users.noreply.github.com>
2021-07-24 09:41:11 -0400

    Updates

---
+++ precache-manifest.eb86ee691442031c08e07d216f6555c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4577890702cd8810c732e587ac3b3fdf",
+    "url": "/reactBartenderApp/index.html"
+  },
+  {
+    "revision": "3d77bae8d7077f4ccd6f",
+    "url": "/reactBartenderApp/static/css/main.db6e84ad.chunk.css"
+  },
+  {
+    "revision": "e1f6309a6f0d3e350015",
+    "url": "/reactBartenderApp/static/js/2.72303b8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactBartenderApp/static/js/2.72303b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d77bae8d7077f4ccd6f",
+    "url": "/reactBartenderApp/static/js/main.db89520d.chunk.js"
+  },
+  {
+    "revision": "a062ef2f1c99b1c9df90",
+    "url": "/reactBartenderApp/static/js/runtime-main.aa5cdcc1.js"
+  },
+  {
+    "revision": "f3ad766494d1f9701717c1a860cd5b10",
+    "url": "/reactBartenderApp/static/media/logo.f3ad7664.svg"
+  }
+]);
